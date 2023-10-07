141020f20d4505bca297fde9eecc6b8ed0e252f1
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-10-07 15:14:18 -0300

    feat: implementando classe user

---
+++ IceGestor.Core/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace IceGestor.Core.Entities;
+public class User : BaseEntity
+{
+    public User(int username, string password, string email)
+    {
+        Username = username;
+        Password = password;
+        Email = email;
+    }
+
+    public int Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
