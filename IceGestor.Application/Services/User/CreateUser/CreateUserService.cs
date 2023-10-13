a97937aea9250bcf6ab0416ad54d89ac2d90c765
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-10-13 13:12:53 -0300

    feat: implementing user service class interface and input and view models

---
+++ IceGestor.Application/Services/User/CreateUser/CreateUserService.cs
@@ -0,0 +1,11 @@
+﻿using IceGestor.CrossCutting.InputModels.User;
+using IceGestor.CrossCutting.ViewModels.User;
+
+namespace IceGestor.Application.Services.User.CreateUser;
+public class CreateUserService : ICreateUserService
+{
+    public Task<UserCreatedViewModel> Execute(CreateUserInputModel request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ IceGestor.CrossCutting/ViewModels/User/UserCreatedViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace IceGestor.CrossCutting.ViewModels.User;
+public class UserCreatedViewModel
+{
+    public string Token { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
