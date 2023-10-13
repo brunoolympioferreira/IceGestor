a3023e69eeb574a3b54053f9b3b0e5750c29ecee
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-10-13 12:38:01 -0300

    feat: implementing authservice

---
+++ IceGestor.Application/Authentication/IAuthService.cs
@@ -0,0 +1,6 @@
+﻿namespace IceGestor.Application.Authentication;
+internal interface IAuthService
+{
+    string GenerateJwtToken(string email, string userName);
+    string ComputeSha256Hash(string password);
+}
