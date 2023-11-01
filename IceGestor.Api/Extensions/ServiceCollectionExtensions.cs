2ae031ea7c88111307b23f4034a8b24ec68e43bb
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-11-01 08:49:41 -0300

    feat: user controller

--- IceGestor.Api/Extensions/ServiceCollectionExtensions.cs
+++ IceGestor.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using IceGestor.Application.Services.User.CreateUser;
+﻿using IceGestor.Application.Authentication;
+using IceGestor.Application.Services.User.CreateUser;
 using IceGestor.Core.RepositoriesInterfaces;
 using IceGestor.Infra.Persistence.Repositories;
 
@@ -12,6 +13,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IUserRepository, UserRepository>();
 
         //Services
+        services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<ICreateUserService, CreateUserService>();
 
         return services;
