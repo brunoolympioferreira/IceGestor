3933caa6f06e806190667bb9c798fe426a3000c0
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-11-10 15:40:28 -0300

    feat: implementando user service

---
+++ IceGestor.Application/Services/User/Login/IUserLoginService.cs
@@ -0,0 +1,8 @@
+﻿using IceGestor.CrossCutting.InputModels.User;
+using IceGestor.CrossCutting.ViewModels.User;
+
+namespace IceGestor.Application.Services.User.Login;
+public interface IUserLoginService
+{
+    Task<LoginUserViewModel> Login(LoginUserInputModel request);
+}
