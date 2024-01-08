09ca4c558849ffe8e26c46404e325849ed7ef4c8
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-01-08 08:12:15 -0300

    feat: implementing create flavor endpoint

--- BackEnd/IceGestor.Api/Controllers/ProductController.cs
+++ BackEnd/IceGestor.Api/Controllers/ProductController.cs
@@ -1,8 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IceGestor.Application.Models.InputModels.Product;
+using IceGestor.Application.Models.ViewModels;
+using IceGestor.Application.Services.Product.Flavor.CreateFlavor;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IceGestor.Api.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class ProductController : ControllerBase
 {
+    private readonly ICreateFlavorService _createFlavorService;
+
+    public ProductController(ICreateFlavorService createFlavorService)
+    {
+        _createFlavorService = createFlavorService;
+    }
+
+    [HttpPost("Flavor")]
+    public async Task<IActionResult> CreateFlavor([FromBody] CreateFlavorInputModel inputModel)
+    {
+        BaseResult result = await _createFlavorService.Execute(inputModel);
+
+        return Ok(result);
+    }
 }
