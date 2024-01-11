e2f8d1842439a4854f42e2c00b3a01a79014c24e
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-01-11 18:16:18 -0300

    implementing endpoint get all flavors

--- BackEnd/IceGestor.Api/Controllers/ProductController.cs
+++ BackEnd/IceGestor.Api/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using IceGestor.Application.Models.InputModels.Product;
 using IceGestor.Application.Models.ViewModels;
 using IceGestor.Application.Services.Product.Flavor.CreateFlavor;
+using IceGestor.Application.Services.Product.Flavor.GetFlavors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IceGestor.Api.Controllers;
@@ -9,16 +10,26 @@ namespace IceGestor.Api.Controllers;
 public class ProductController : ControllerBase
 {
     private readonly ICreateFlavorService _createFlavorService;
+    private readonly IGetFlavorsService _getFlavorsService;
 
-    public ProductController(ICreateFlavorService createFlavorService)
+    public ProductController(ICreateFlavorService createFlavorService, IGetFlavorsService getFlavorsService)
     {
         _createFlavorService = createFlavorService;
+        _getFlavorsService = getFlavorsService;
     }
 
     [HttpPost("Flavor")]
     public async Task<IActionResult> CreateFlavor([FromBody] FlavorInputModel inputModel)
     {
         BaseResult result = await _createFlavorService.Execute(inputModel);
+
+        return Ok(result);
+    }
+
+    [HttpGet("flavor")]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _getFlavorsService.GetAll();
 
         return Ok(result);
     }
