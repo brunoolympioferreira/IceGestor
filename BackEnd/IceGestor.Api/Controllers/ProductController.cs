29ea227208608bb720c0a53da8a4acc81673fbe7
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-01-28 11:45:15 -0300

    feat: add prop authorize na product controller

--- BackEnd/IceGestor.Api/Controllers/ProductController.cs
+++ BackEnd/IceGestor.Api/Controllers/ProductController.cs
@@ -3,11 +3,13 @@ using IceGestor.Application.Models.ViewModels;
 using IceGestor.Application.Services.Product.Flavor.CreateFlavor;
 using IceGestor.Application.Services.Product.Flavor.GetFlavors;
 using IceGestor.Application.Services.Product.Flavor.UpdateFlavor;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IceGestor.Api.Controllers;
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class ProductController : ControllerBase
 {
     private readonly ICreateFlavorService _createFlavorService;
