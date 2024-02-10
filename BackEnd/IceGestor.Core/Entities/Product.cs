d7908479dcbf1e0efa94b61debcd569ef34fcf1a
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-02-10 16:38:51 -0300

    implementando product core

---
+++ BackEnd/IceGestor.Core/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace IceGestor.Core.Entities;
+public class Product : BaseEntity
+{
+    public Product(decimal amount, int idFlavor, int idCategory)
+    {
+        Amount = amount;
+        IdFlavor = idFlavor;
+        IdCategory = idCategory;
+
+        CreatedAt = DateTime.Now;
+    }
+
+    public decimal Amount { get; private set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public int IdFlavor { get; set; }
+    public int IdCategory { get; set; }
+
+    public Flavor Flavor { get; set; }
+    public Category Category { get; set; }
+}
